307eb131cf61ea3d12dd345d343e66b45ed2c869
Richard Yin <richard.hongyi.yin@gmail.com>
2015-09-21 13:42:41 -0400

    Client now sends a basic message, prints the response, and exits

--- src/comp3203/assignment1/client/FileTransferClient.java
+++ src/comp3203/assignment1/client/FileTransferClient.java
@@ -1,4 +1,10 @@
 package comp3203.assignment1.client;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 /**
  * Main class for the file transfer program, client side.
  * @author Richard
@@ -6,13 +12,28 @@ package comp3203.assignment1.client;
  */
 
 public class FileTransferClient {
+	
+	Socket connection;
+	ObjectOutputStream outputStream;
+	ObjectInputStream inputStream;
 
-	public FileTransferClient() {
-		
+	public FileTransferClient(String host, int port) {
+		try {
+			connection = new Socket(host, port);
+			outputStream = new ObjectOutputStream(connection.getOutputStream());
+			inputStream = new ObjectInputStream(connection.getInputStream());
+			outputStream.writeObject("ping"); // test message
+			System.out.println(inputStream.readObject());
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
-		new FileTransferClient();
+		String host = args[0];
+		int port = Integer.parseInt(args[1]);
+		// new FileTransferClient(host, port);
 	}
 
 }
