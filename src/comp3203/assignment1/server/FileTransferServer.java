b939b3889724d47240ea4483149fc9f3a00d46b0
MarkWilkes <markeous.wilkes@gmail.com>
2015-09-21 14:32:02 -0400

    Refactored the Server

--- src/comp3203/assignment1/server/FileTransferServer.java
+++ src/comp3203/assignment1/server/FileTransferServer.java
@@ -1,7 +1,7 @@
 package comp3203.assignment1.server;
 /**
  * Main class for the file transfer program, client side.
- * @author Richard
+ * @author Mark
  *
  */
 
@@ -20,72 +20,81 @@ public class FileTransferServer {
 	//Streams
 	private static ObjectInputStream in;
 	private static ObjectOutputStream out;
+	private static Socket clientS;
+	private static String line;
 	
 	//working directory
 	//private static String wd;
 	
 	public FileTransferServer() {
+		line = null;
+		clientS = null;
+		out = null;
+		in = null;
+		
 		try {
 			s = new ServerSocket(0);
 		} catch (IOException e) {
 			System.out.println("Couldn't listen on a new port");
 			System.exit(-1);
 		}
-		
+
 		System.out.println("Listening on port: " + s.getLocalPort());
+	}
+	
+	public static int ClientConnection(){
+		try {
+			clientS = s.accept();
+			System.out.println("Handling client at " + 
+					clientS.getInetAddress().getHostAddress() + " on port " +
+					clientS.getPort());
+		} catch (IOException e) {
+			System.out.println("Accept failed " + s.getLocalPort());
+			return(-1);
+		}
+		
+		try{
+			in = new ObjectInputStream(clientS.getInputStream());
+			out = new ObjectOutputStream(clientS.getOutputStream());
+		} catch (IOException e){
+			e.printStackTrace();
+			return(-1);
+		}
+		
+		while(true){
+			try{
+				line = in.readObject().toString();
+				if(line.equals("bye")){
+					break;
+				}
+				out.writeObject(line);
+			} catch (IOException e){
+				e.printStackTrace();
+				break;
+			} catch (ClassNotFoundException e){
+				e.printStackTrace();
+				break;
+			}
+		}
+		
+		try{
+			clientS.close();
+		} catch(IOException e){
+			System.out.println("Failed to close client connection");
+			return(-1);
+		}
+		return(0);
 	}
 
 	public static void main(String[] args) {
 		new FileTransferServer();
 		
 		boolean stop = false;
-		String line = null;
-		Socket clientS = null;
-		out = null;
-		in = null;
 		
 		while(!stop){
-			try {
-				clientS = s.accept();
-				System.out.println("Handling client at " + 
-						clientS.getInetAddress().getHostAddress() + " on port " +
-						clientS.getPort());
-			} catch (IOException e) {
-				System.out.println("Accept failed " + s.getLocalPort());
-				System.exit(-1);
-			}
-			
-			try{
-				in = new ObjectInputStream(clientS.getInputStream());
-				out = new ObjectOutputStream(clientS.getOutputStream());
-			} catch (IOException e){
-				e.printStackTrace();
-				System.exit(-1);
-			}
-			
-			while(true){
-				try{
-					line = in.readObject().toString();
-					if(line.equals("bye")){
-						break;
-					}
-					out.writeObject(line);
-				} catch (IOException e){
-					e.printStackTrace();
-					break;
-				} catch (ClassNotFoundException e){
-					e.printStackTrace();
-					break;
-				}
-			}
-			
-			try{
-				clientS.close();
-			} catch(IOException e){
-				System.out.println("Failed to close client connection");
-				System.exit(-1);
-			}
+			if(ClientConnection() == -1)
+				stop = true;
 		}
 	}
-
+	
 }
