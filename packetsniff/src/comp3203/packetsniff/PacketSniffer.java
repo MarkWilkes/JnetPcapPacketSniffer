5560af8d587dfed0546e00d4d236fad951dac946
Richard Yin <richard.hongyi.yin@gmail.com>
2015-11-28 20:19:35 -0500

    Add main and empty window

---
+++ packetsniff/src/comp3203/packetsniff/PacketSniffer.java
@@ -0,0 +1,11 @@
+package comp3203.packetsniff;
+
+import comp3203.packetsniff.ui.MainWindow;
+
+public class PacketSniffer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new MainWindow().setVisible(true);
+	}
+}
---
+++ packetsniff/src/comp3203/packetsniff/ui/MainWindow.java
@@ -0,0 +1,11 @@
+package comp3203.packetsniff.ui;
+
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+
+	public MainWindow() {
+		super("Packet Sniffer");
+		setSize(800, 600);
+	}
+}
